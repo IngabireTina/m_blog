278c13415a8b44819f5d2435daf95c4c2ec74dc9
IngabireTina <ingabire067@gmail.com>
2021-04-04 15:22:46 -0400

    corrected some errors

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export MAIL_USERNAME=tharcissieidufashe@gmai.com
-export MAIL_PASSWORD=ntakarakorwa123
+export MAIL_USERNAME=ithinah07@gmail.com
+export MAIL_PASSWORD=Ingabire@067
 
 python3.6 manage.py server
